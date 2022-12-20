4e0389ccadce15a96c76efb4c02b1429c98314c2
johnxiler <johnxiler@gmail.com>
2022-12-20 19:34:38 +0800

    add hot reloader in django setting and middlewares

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,41 @@
+from django.conf import settings
 from django.db import models
-
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 # Create your models here.
+User = settings.AUTH_USER_MODEL
 
+
+class ObjectViewed(models.Model):
+    user = models.ForeignKey(User, blank=True, null=True,
+                             on_delete=models.CASCADE)  # User instance
+    # ip_address = models.CharField(max_length=220, blank=True, null=True) # IP Address
+    content_type = models.ForeignKey(
+        ContentType, on_delete=models.CASCADE)  # Product, Post, User, Image
+    # Product id, Post id, User id, Image id
+    object_id = models.PositiveIntegerField()
+    # Product instance, Post instance, User instance, Image instance
+    content_object = GenericForeignKey('content_type', 'object_id')
+    timestamp = models.DateTimeField(auto_now_add=True)  # timestamp
+
+    def __str__(self):
+        return "%s viewed on %s" % (self.content_object, self.timestamp)
+
+    class Meta:
+        ordering = ['-timestamp']  # most recent saved show up first
+        verbose_name = 'Object Viewed'
+        verbose_name_plural = 'Objects Viewed'
+
+
+# class Question(models.Model):
+#     question_text = models.CharField(max_length=200)
+#     pub_date = models.DateTimeField('date published')
+
+
+# class Choice(models.Model):
+#     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+#     choice_text = models.CharField(max_length=200)
+#     votes = models.IntegerField(default=0)
 # class Product(models.Model):
 #     name = models.CharField(max_length=255)
 #     description = models.TextField()
